5172cb6d18464b3797ec4fc1264bfb7178ed2d19
lindemberg <lindembergjnr@gmail.com>
2023-08-10 23:39:10 -0300

    começando realmente

--- React_Exercicios/src/App.tsx
+++ React_Exercicios/src/App.tsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
 import './App.css'
+import axios from 'axios';
 
+
+const Api = axios.create({
+  baseURL: ""
+})
 const Test = () => {
 
   return (
     <h1>Tarefa teste</h1>
   )
   }
-const Lista = () => {
-  const [tarefas, setTarefas] = useState([
-    "Tarefa 1", "Tarefa 2", "Tarefa 3",
-  ]);
 
+const Back_Cadastro = () => {
+  return(
+  <button>Cadastro</button>
+  )
+}
+const Back_Login = () => {
+  return(
+    <button>Login</button>
+  )
+}
+const Lista = (props: any) => {
+ 
   return (
 
     <div>
-      {tarefas.map((item, index) => <li key={index}>{item}</li>)
+      {props.item.map((item: string, index: number) => <li key={index}>{item}</li>)
        }
         </div>
 
@@ -23,12 +36,17 @@ const Lista = () => {
 }
 
 const App = () => {
- 
+  const [tarefas, setTarefas] = useState([
+    "Tarefa 1", "Tarefa 2", "Tarefa 3",
+  ]);
 
   return (
     <>
       <Test />
-      <Lista />
+      <Lista item={tarefas}/>
+      <Back_Cadastro/>
+      <Back_Login />
+      
     </>
   )
 }
